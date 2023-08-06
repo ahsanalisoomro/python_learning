f8a37967455c8c48fb792098c7bdbb24f7b155cd
Ahsan Ali <97788278+ahsanalisoomro@users.noreply.github.com>
2023-08-06 06:51:15 +0000

    create some program

---
+++ basic/08_if_else_Condition.py
@@ -0,0 +1,10 @@
+
+
+# now i will create program for Voting Machine who check is Eligible or not
+
+print("Welcome To Votting Area")
+age = int(input("Enter Your Age: "))
+if(age>18):
+    print("You Are Eligible for Vote")
+else:
+    print("Your are Child See And Watch the process ")
--- basic/first_program.py
+++
@@ -1,3 +0,0 @@
-print("Hello World ...")
-print("this is ahsan ali you may know because i start contributung work on linkedin and other social media platform")
-print("And this is my first program")
