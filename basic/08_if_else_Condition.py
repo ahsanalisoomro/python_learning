cdf67a3776c7ed199dea9014df7ee058cc3ed86e
Ahsan Ali <97788278+ahsanalisoomro@users.noreply.github.com>
2023-08-06 07:24:14 +0000

    create a program for if-else

--- basic/08_if_else_Condition.py
+++ basic/08_if_else_Condition.py
@@ -8,3 +8,22 @@ if(age>18):
     print("You Are Eligible for Vote")
 else:
     print("Your are Child See And Watch the process ")
+
+
+print("_______________________________________________")
+
+print("This program is Decision Oriented Program ..")
+
+Bugdet = int(input("Enter Your Expencess :"))
+if(Bugdet>1000 and Bugdet<2000):
+    print("We are Eat Karahi in Evening")
+    
+if(Bugdet>2000 and Bugdet<5000):
+    print("We Are Eat Biryani With Raita On Friday")
+if(Bugdet>5000):
+    print("We Are Decide BBQ Platter With Qabuli Pulao")
+else:
+ print("Paisa Hy Nahi Batien Suno Janab Ki")
+
+
+ 
