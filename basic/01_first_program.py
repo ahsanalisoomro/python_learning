7d0249557a66f69bedd007428251584d648adc5f
Ahsan Ali <97788278+ahsanalisoomro@users.noreply.github.com>
2023-08-06 06:31:21 +0000

    change first file name

---
+++ basic/01_first_program.py
@@ -0,0 +1,3 @@
+print("Hello World ...")
+print("this is ahsan ali you may know because i start contributung work on linkedin and other social media platform")
+print("And this is my first program")
