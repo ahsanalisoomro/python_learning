5f119a00b6fe50305ad501182ea132e6b2864819
Ahsan Ali <97788278+ahsanalisoomro@users.noreply.github.com>
2023-08-17 08:27:04 +0000

    indexing

---
+++ basic/14_indexing.py
@@ -0,0 +1,23 @@
+# Python3 program for demonstration  
+# of list index() method
+  
+list1 = [1, 2, 3, 4, 1, 1, 1, 4, 5]
+  
+# Will print the index of '4' in list1
+print(list1.index(4))
+  
+list2 = ['cat', 'bat', 'mat', 'cat', 'pet']
+  
+# Will print the index of 'cat' in list2 
+print(list2.index('cat'))
+
+
+# Python3 program for demonstration 
+# of index() method
+  
+list1 = [1, 2, 3, 4, 1, 1, 1, 4, 5]
+  
+# Will print index of '4' in sublist
+# having index from 4 to 8.
+print(list1.index(4, 4, 8))
+
