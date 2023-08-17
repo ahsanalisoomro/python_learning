c2a88f9187411f53fc9a7ea4222694fd0006b9b8
Ahsan Ali <97788278+ahsanalisoomro@users.noreply.github.com>
2023-08-17 08:29:58 +0000

    indexing

--- basic/14_indexing.py
+++ basic/14_indexing.py
@@ -21,3 +21,9 @@ list1 = [1, 2, 3, 4, 1, 1, 1, 4, 5]
 # having index from 4 to 8.
 print(list1.index(4, 4, 8))
 
+# Will print index of '1' in sublist
+# having index from 1 to 7.
+print(list1.index(1, 1, 7))
+  
+list2 = ['cat', 'bat', 'mat', 'cat', 
+         'get', 'cat', 'sat', 'pet']
