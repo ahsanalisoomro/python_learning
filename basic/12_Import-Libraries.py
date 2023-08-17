83e7ca316f15a158391b1ebcb047e13016e8463f
Ahsan Ali <97788278+ahsanalisoomro@users.noreply.github.com>
2023-08-17 08:16:45 +0000

    try libraries

--- basic/12_Import-Libraries.py
+++ basic/12_Import-Libraries.py
@@ -3,3 +3,13 @@ import math
 import statistics
 print('value of pi is:' ,math.pi)
 print("Value of square :",math.log10)
+
+#importing sqrt() and factorial from the
+# module math
+from math import sqrt, factorial
+  
+# if we simply do "import math", then
+# math.sqrt(16) and math.factorial()
+# are required.
+print(sqrt(16))
+print(factorial(6))
