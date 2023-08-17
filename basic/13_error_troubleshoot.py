2314842b8eb082367d4b12d7726f93eb057eb381
Ahsan Ali <97788278+ahsanalisoomro@users.noreply.github.com>
2023-08-17 08:23:07 +0000

    trouble shoot

---
+++ basic/13_error_troubleshoot.py
@@ -0,0 +1,7 @@
+# initialize the amount variable
+amount = 10000
+ 
+# check that You are eligible to
+#  purchase Dsa Self Paced or not
+# if(amount > 2999):
+# print("You are eligible to purchase Dsa Self Paced")
