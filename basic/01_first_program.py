50295c6a74c8b6c4f75729025b5c242e1332e5fe
Ahsan Ali <97788278+ahsanalisoomro@users.noreply.github.com>
2023-08-18 23:57:18 +0500

    Update 01_first_program.py

--- basic/01_first_program.py
+++ basic/01_first_program.py
@@ -1,3 +1,15 @@
 print("Hello World ...")
 print("this is ahsan ali you may know because i start contributung work on linkedin and other social media platform")
 print("And this is my first program")
+
+
+# This program adds two numbers
+
+num1 = 1.5
+num2 = 6.3
+
+# Add two numbers
+sum = num1 + num2
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
