d3530be19ab318fb2d0db24c675beb63b15062bc
Ahsan Ali <97788278+ahsanalisoomro@users.noreply.github.com>
2023-08-29 22:40:25 +0500

    Update 11_Loops.py

--- Intro to Python/11_Loops.py
+++ Intro to Python/11_Loops.py
@@ -78,5 +78,15 @@ for i in range(len(keys)):
 print(res_dict)
 
 
+# s: store sum of all numbers
+s = 0
+n = int(input("Enter number "))
+# run loop n times
+# stop: n+1 (because range never include stop number in result)
+for i in range(1, n + 1, 1):
+    # add current number to sum variable
+    s += i
+print("\n")
+print("Sum is: ", s)
 
 
