fcb49f9b785adb7cc7d798751ddac149456aaafc
Ahsan Ali <97788278+ahsanalisoomro@users.noreply.github.com>
2023-08-29 22:38:31 +0500

    Update 11_Loops.py

--- Intro to Python/11_Loops.py
+++ Intro to Python/11_Loops.py
@@ -65,5 +65,18 @@ for count in range(2, 30, 3):
   print(count)
 
 
+# use dictionary using loops
+
+keys = ['Ten', 'Twenty', 'Thirty']
+values = [10, 20, 30]
+
+# empty dictionary
+res_dict = dict()
+
+for i in range(len(keys)):
+    res_dict.update({keys[i]: values[i]})
+print(res_dict)
+
+
 
 
